3404f325bd192e09a8944bf0781b1b64771603d5
Antonia Dimitrova <asdimitrova88@gmail.com>
2017-05-21 17:34:15 +0300

    -

---
+++ tests/MyBankAccountTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Antonia Dimitrova
+ * Date: 21.5.2017 г.
+ * Time: 17:32
+ */
+use PHPUnit\Framework\TestCase;
+require __DIR__ . "/../src/MyBankAccount.php";
+
+class MyBankAccountTest extends TestCase
+{
+
+}
