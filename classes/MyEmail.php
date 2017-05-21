77d029276ae90e0fd33c5e33cbe8aebc9d3bc66d
Antonia Dimitrova <asdimitrova88@gmail.com>
2017-05-21 16:27:13 +0300

    -

---
+++ classes/MyEmail.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antonia Dimitrova
+ * Date: 21.5.2017 г.
+ * Time: 16:26
+ */
+declare(strict_types=1);
+
+final class MyEmail
+{
+    private $email;
+
+    private function __construct(string $email)
+    {
+        $this->ensureIsValidEmail($email);
+
+        $this->email = $email;
+    }
+
+    public static function fromString(string $email): self
+    {
+        return new self($email);
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
+    private function ensureIsValidEmail(string $email): void
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '"%s" is not a valid email address',
+                    $email
+                )
+            );
+        }
+    }
+}
