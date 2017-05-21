2504813faa2d3d4c405481e42554369b1be57411
Antonia Dimitrova <asdimitrova88@gmail.com>
2017-05-21 16:29:24 +0300

    -

---
+++ tests/MyEmailTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antonia Dimitrova
+ * Date: 21.5.2017 г.
+ * Time: 16:27
+ */
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * @covers MyEmail
+ */
+class MyEmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress(): void
+    {
+        $this->assertInstanceOf(
+            MyEmail::class,
+            MyEmail::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        MyEmail::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            'user@example.com',
+            MyEmail::fromString('user@example.com')
+        );
+    }
+}
