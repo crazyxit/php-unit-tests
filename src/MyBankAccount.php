7af5ee40323922699e813754ea764134ff52a8b2
Antonia Dimitrova <asdimitrova88@gmail.com>
2017-05-21 17:32:15 +0300

    -

--- src/MyBankAccount.php
+++ src/MyBankAccount.php
@@ -8,12 +8,44 @@
 class MyBankAccount{
     private $age;
     private $sum;
+    private $interest = 0;
 
     private $minSum = 100;
     private $maxSum = 10000;
 
-    public function __construct($age, $sum)  {
+    public function __construct(int $age, float $sum)  {
         $this->age = $age;
         $this->sum = $sum;
+
+        $this->calcInterest();
+    }
+
+    public function getSum(){
+        return $this->sum;
+    }
+
+    public function getInterest(){
+        return $this->interest;
+    }
+
+    public function calcInterest(){
+        if($this->age >= 60){
+            $this->interest = 0.02; //2%
+        }
+        else if($this->age >= 18){
+            if ($this->sum >= $this->maxSum){
+                $this->interest = 0.015; //1.5%
+            }
+            else if ($this->sum >= 1000){
+                $this->interest = 0.013; //1.3%
+            }
+            else if ($this->sum >= $this->minSum){
+                $this->interest = 0.01; //1.0%
+            }
+        }
+    }
+
+    public function getAmount(){
+        return $this->sum + ($this->sum * $this->interest);
     }
 }
